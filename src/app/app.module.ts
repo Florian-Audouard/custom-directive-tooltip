09f3b64262fa4e6acaa533b60169d9bb48885845
Florian-Audouard <45578522+Florian-Audouard@users.noreply.github.com>
2023-04-20 17:55:37 +1100

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomToolTipComponent } from './custom-tool-tip/custom-tool-tip.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomToolTipComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/custom-tool-tip/custom-tool-tip.component.ts
@@ -0,0 +1,51 @@
+import { ContentChild, Directive, ElementRef, QueryList } from '@angular/core';
+function getCoord( elementParent:HTMLElement,tooltip:HTMLElement){
+  const boxParent = elementParent.getBoundingClientRect()
+  const boxToolTip = tooltip.getBoundingClientRect()
+  let left = (boxParent.left  + (boxParent.width/2)) - (boxToolTip.width/2)
+  left = (boxParent.left - (boxToolTip.width/2)) + ((boxParent.width-boxToolTip.width)/2)
+  console.log(elementParent.innerText)
+  let top = 0;
+  if(boxParent.y > boxToolTip.height){
+    top = boxParent.top - boxToolTip.height
+  }
+  else{
+    top = boxParent.top + boxParent.height
+  }
+  return [left,top]
+    
+}
+
+
+@Directive({
+  selector: '[customToolTip]'})
+export class CustomToolTipComponent {
+  @ContentChild('customToolTip') 
+  element !: ElementRef
+  parent : HTMLElement
+  constructor(private eleRef: ElementRef) {
+    this.parent = eleRef.nativeElement
+}
+
+
+
+ngAfterViewInit(){
+  const currentElement = this.element.nativeElement;
+  const pos = getCoord(this.parent,currentElement)
+  currentElement.style.position = "absolute";
+  currentElement.style.left = `${pos[0]}px`
+  currentElement.style.top = `${pos[1]}px`
+  currentElement.hidden = true;
+  this.parent.addEventListener("mouseover",function t(e:any){
+    console.log("allo")
+    currentElement.hidden = false;
+  })
+  this.parent.addEventListener("mouseout",function t(e:any){
+    currentElement.hidden = true;
+  })
+  
+  
+}
+
+
+}
